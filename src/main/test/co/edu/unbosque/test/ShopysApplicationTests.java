df7623032e1dc55acb93b038db5d9f2df12cc583
Stevensup <buseche@unbosque.edu.co>
2023-11-27 00:55:40 -0500

    ajuste

--- src/main/test/co/edu/unbosque/test/ShopysApplicationTests.java
+++ src/main/test/co/edu/unbosque/test/ShopysApplicationTests.java
@@ -1,6 +1,6 @@
 package co.edu.unbosque.test;
 
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -11,37 +11,19 @@ import co.edu.unbosque.model.Compra_inventario;
 import co.edu.unbosque.model.ProductoDTO;
 
 public class ShopysApplicationTests {
-	private InventarioBean inventarioBean;
-    private AdminProducto adminProductoMock;
-    private Compra_inventario inventarioMock;
-
-
-    @Before
-    public void setUp() {
-        // Inicializar los mocks y la clase bajo prueba
-        adminProductoMock = mock(AdminProducto.class);
-        inventarioMock = mock(Compra_inventario.class);
-        inventarioBean = new InventarioBean();
-    }
 
     @Test
-    public void testInsertar() {
-        // Configurar el comportamiento esperado para el mock de AdminProducto
-        ProductoDTO productoExistente = new ProductoDTO("ProductoExistente", "Descripción", 10.0, 20, "Categoria");
-        when(adminProductoMock.buscar("ProductoExistente")).thenReturn(productoExistente);
+    public void testInsertarProducto() {
+        // Create an instance of the ShopysApplication class
+        ShopysApplication shopysApplication = new ShopysApplication();
 
-        // Configurar el comportamiento esperado para el mock de Inventario
-        when(adminProductoMock.editar(eq("ProductoExistente"), any(ProductoDTO.class))).thenReturn(true);
+        // Create a product to insert
+        Product product = new Product("Example Product", 10.99);
 
-        // Llamada al método bajo prueba
-        inventarioBean.insertar();
+        // Insert the product
+        boolean result = shopysApplication.insertProduct(product);
 
-        // Verificar que las interacciones esperadas ocurrieron
-        verify(adminProductoMock).buscar("ProductoExistente");
-        verify(adminProductoMock).editar(eq("ProductoExistente"), any(ProductoDTO.class));
-        verify(inventarioMock).ingresarinv(any(CompraInventarioDTO.class));
-
-        // Puedes agregar más aserciones según sea necesario
+        // Assert that the product was inserted successfully
+        assertTrue(result);
     }
 }
-
