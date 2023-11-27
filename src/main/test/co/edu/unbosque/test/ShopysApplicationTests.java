ef4a026ad237e11926046a1546d4beb3ed13c807
XxSantiagoxX15 <santiagomelo15@hotmail.com>
2023-11-27 01:14:21 -0500

    Junits

--- src/main/test/co/edu/unbosque/test/ShopysApplicationTests.java
+++ src/main/test/co/edu/unbosque/test/ShopysApplicationTests.java
@@ -1,29 +1,36 @@
 package co.edu.unbosque.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-import co.edu.unbosque.beans.InventarioBean;
 import co.edu.unbosque.model.AdminProducto;
-import co.edu.unbosque.model.Compra_inventario;
 import co.edu.unbosque.model.ProductoDTO;
 
 public class ShopysApplicationTests {
 
-    @Test
-    public void testInsertarProducto() {
-        // Create an instance of the ShopysApplication class
-        ShopysApplication shopysApplication = new ShopysApplication();
+	@Test
+	public void testInsertarProducto() {
+		// Create an instance of the ShopysApplication class
+		AdminProducto producto = new AdminProducto();
 
-        // Create a product to insert
-        Product product = new Product("Example Product", 10.99);
+		// Create a product to insert
+		ProductoDTO product = new ProductoDTO(3, "Example Product", "Descripcion prueba", 10.99, 0, "prueba");
 
-        // Insert the product
-        boolean result = shopysApplication.insertProduct(product);
+		boolean result = producto.insertar(product);
 
-        // Assert that the product was inserted successfully
-        assertTrue(result);
-    }
+		assertTrue(result);
+	}
+
+	@Test
+	public void testListarxinventario() {
+		AdminProducto producto = new AdminProducto();
+
+		ProductoDTO product = new ProductoDTO("Example Product", "Descripcion prueba", 10.99, 0, "prueba");
+		producto.insertar(product);
+
+		ProductoDTO result = producto.buscar(3);
+		assertEquals(product, result);
+	}
 }
